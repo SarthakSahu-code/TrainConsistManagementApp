ebcd0dd274d028f2e6c7524afd341d571526cd8b
SarthakSahu-code <ss0347@srmist.edu.in>
2026-04-14 15:40:43 +0530

    Sarthak added UC3

--- App/src/TrainConsistManagementApp.java
+++ App/src/TrainConsistManagementApp.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * ========================================================
@@ -9,7 +11,7 @@ import java.util.List;
  * * It contains multiple Use Cases demonstrating Java collections.
  * *
  * * @author Developer
- * @version 2.0
+ * @version 3.0
  */
 public class TrainConsistManagementApp {
 
@@ -67,6 +69,34 @@ public class TrainConsistManagementApp {
 
         // Print final list state
         System.out.println("Final passenger bogies consist : " + passengerBogies);
+        System.out.println("\n");
+
+
+        // ========================================================
+        // USE CASE 3: Track Unique Bogie IDs
+        // ========================================================
+
+        System.out.println("=========================================");
+        System.out.println(" UC3 - Track Unique Bogie IDs ");
+        System.out.println("=========================================\n");
+
+        // Create a Set to store unique bogie IDs
+        // HashSet stores only unique values
+        Set<String> bogies = new HashSet<>();
+
+        // ---- ADD IDs (including duplicates) ----
+        // add() inserts bogie IDs into the set
+        bogies.add("BG101");
+        bogies.add("BG102");
+        bogies.add("BG103");
+        bogies.add("BG104");
+
+        // Duplicate entries will be ignored internally by HashSet
+        bogies.add("BG101"); // Duplicate entry
+        bogies.add("BG102"); // Duplicate entry
+
+        // Print the final set to observe that duplicates are removed automatically
+        System.out.println("Unique Bogie IDs : " + bogies);
         System.out.println();
 
     }
